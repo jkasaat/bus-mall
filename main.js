16caaf03fb869837ffe031d3b5370c7f2943b787
Jalaluddin Kasaat <jalaluddinkasaat@Jalaluddins-MacBook-Pro.local>
2018-08-03 23:35:58 -0400

    added Chart and Local Storage

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ let elImage1 = document.getElementById('random-image1');
 let elImage2 = document.getElementById('random-image2');
 let elImage3 = document.getElementById('random-image3');
 
-//declaring image index so that for global access
+//declaring image index for global access
 let imageIndex1;
 let imageIndex2;
 let imageIndex3;
@@ -21,18 +21,26 @@ let Image = function(index, name, filePath, showed, clicked) {
 }
 
 //Instanitating new image objects. Clicked and showed are assigned a value of 0.
-let image1 = new Image(0, 'thanos', 'https://raw.githubusercontent.com/CodePartnersMD/MD201-01/master/weeks-5-6/lecture-10/code-demo/assets/thanos.jpg', 0, 0);
-let image2 = new Image(1, 'thor', 'https://raw.githubusercontent.com/CodePartnersMD/MD201-01/master/weeks-5-6/lecture-10/code-demo/assets/thor.jpg', 0, 0);
-let image3 = new Image(2, 'deadpool', 'https://raw.githubusercontent.com/CodePartnersMD/MD201-01/master/weeks-5-6/lecture-10/code-demo/assets/deadpool.jpeg', 0, 0);
-let image4 = new Image(3, 'black-panther', 'https://raw.githubusercontent.com/CodePartnersMD/MD201-01/master/weeks-5-6/lecture-10/code-demo/assets/black-panther.jpg', 0, 0);
-let image5 = new Image(4, 'breakfast', 'https://raw.githubusercontent.com/CodePartnersMD/MD201-01/master/weeks-5-6/lecture-10/assets/breakfast.jpg', 0, 0);
-let image6 = new Image(5, 'dog-duck', 'https://raw.githubusercontent.com/CodePartnersMD/MD201-01/master/weeks-5-6/lecture-10/assets/dog-duck.jpg', 0, 0);
-let image7 = new Image(6, 'drstrange', 'https://raw.githubusercontent.com/CodePartnersMD/MD201-01/master/weeks-5-6/lecture-10/code-demo/assets/drstrange.jpg', 0, 0);
-let image8 = new Image(7, 'hulk', 'https://raw.githubusercontent.com/CodePartnersMD/MD201-01/master/weeks-5-6/lecture-10/code-demo/assets/hulk.jpg', 0, 0);
-let image9 = new Image(8, 'wolverine', 'https://raw.githubusercontent.com/CodePartnersMD/MD201-01/master/weeks-5-6/lecture-10/code-demo/assets/wolverine.jpg', 0, 0);
+
+if(localStorage.imageObject) {
+    imageArray = JSON.parse(localStorage.getItem("imageObject"))
+}
+else {
+
+let image1 = new Image(0, 'Thanos', 'https://raw.githubusercontent.com/CodePartnersMD/MD201-01/master/weeks-5-6/lecture-10/code-demo/assets/thanos.jpg', 0, 0);
+let image2 = new Image(1, 'Thor', 'https://raw.githubusercontent.com/CodePartnersMD/MD201-01/master/weeks-5-6/lecture-10/code-demo/assets/thor.jpg', 0, 0);
+let image3 = new Image(2, 'Deadpool', 'https://raw.githubusercontent.com/CodePartnersMD/MD201-01/master/weeks-5-6/lecture-10/code-demo/assets/deadpool.jpeg', 0, 0);
+let image4 = new Image(3, 'Black-panther', 'https://raw.githubusercontent.com/CodePartnersMD/MD201-01/master/weeks-5-6/lecture-10/code-demo/assets/black-panther.jpg', 0, 0);
+let image5 = new Image(4, 'Breakfast', 'https://raw.githubusercontent.com/CodePartnersMD/MD201-01/master/weeks-5-6/lecture-10/assets/breakfast.jpg', 0, 0);
+let image6 = new Image(5, 'Dog-duck', 'https://raw.githubusercontent.com/CodePartnersMD/MD201-01/master/weeks-5-6/lecture-10/assets/dog-duck.jpg', 0, 0);
+let image7 = new Image(6, 'Drstrange', 'https://raw.githubusercontent.com/CodePartnersMD/MD201-01/master/weeks-5-6/lecture-10/code-demo/assets/drstrange.jpg', 0, 0);
+let image8 = new Image(7, 'Hulk', 'https://raw.githubusercontent.com/CodePartnersMD/MD201-01/master/weeks-5-6/lecture-10/code-demo/assets/hulk.jpg', 0, 0);
+let image9 = new Image(8, 'Wolverine', 'https://raw.githubusercontent.com/CodePartnersMD/MD201-01/master/weeks-5-6/lecture-10/code-demo/assets/wolverine.jpg', 0, 0);
 
 //pushing new image objects to image array
 imageArray.push(image1, image2, image3,image4, image5, image6,image7, image8, image9);
+
+};
 
 // Creating a function that will help me select random images that weren't shown in the previous slide.
 let randomImage = function() {
@@ -80,13 +88,115 @@ let randomImage = function() {
 }
    
 // variable to hold total selections, globally
-let totalSelections = 0;
+let totalSelections = localStorage.getItem("total" || 0);
+
+// let totalSelections = 0;
+// if(localStorage.getItem('total')){
+//     totalSelections = localStorage.getItem('total');
+// } 
+
 let totalSelected = document.getElementById('total-selected');
+
+
+function chartLabels(prop) {
+    let labels = []
+    for(let i=0; i<imageArray.length; i++) {
+
+        labels.push(imageArray[i][prop])
+    }
+return labels
+}
+
+function createChart () {
+    var ctx = document.getElementById("myChart").getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: chartLabels('name'),
+        datasets: [{
+            label: ' Number of Votes',
+            data: chartLabels('clicked'),
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)'
+
+
+                
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(255,99,132,1)',
+                'rgba(255,99,132,1)',
+                'rgba(255,99,132,1)',
+                'rgba(255,99,132,1)',
+                'rgba(255,99,132,1)',
+                'rgba(255,99,132,1)',
+                'rgba(255,99,132,1)',
+                'rgba(255,99,132,1)'
+            ],
+            borderWidth: 1,
+            stack: 1
+        },
+        {
+            label: ' Times shown',
+            data: chartLabels('showed'),
+            backgroundColor: [
+                'rgba(245, 240, 80, 0.9)',
+                'rgba(245, 240, 80, 0.9)',
+                'rgba(245, 240, 80, 0.9)',
+                'rgba(245, 240, 80, 0.9)',
+                'rgba(245, 240, 80, 0.9)',
+                'rgba(245, 240, 80, 0.9)',
+                'rgba(245, 240, 80, 0.9)',
+                'rgba(245, 240, 80, 0.9)',
+                'rgba(245, 240, 80, 0.9)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(255,99,132,1)',
+                'rgba(255,99,132,1)',
+                'rgba(255,99,132,1)',
+                'rgba(255,99,132,1)',
+                'rgba(255,99,132,1)',
+                'rgba(255,99,132,1)',
+                'rgba(255,99,132,1)',
+                'rgba(255,99,132,1)'
+
+
+
+            ],
+            borderWidth: 1,
+            stack: 2
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+
+}
+
+
+
+
 
 // creating event handler to track number of times image is clicked
 let imageClick = function(e) {
     // FIRST, RECORD THE CLICK AND THEN SHOW NEXT THREE IMAGES
-
+   
     //accessing clicked property on random image and incrementing by 1 each time clicked
     if(e.target.id == 'random-image1')              // this is basically checking which image was clicked so the corresponding counter to be incremented
         imageIndex1.clicked += 1;
@@ -131,10 +241,20 @@ let imageClick = function(e) {
         let res = document.getElementById('result');
         res.appendChild(ul);
         res.style.display = 'block';
+        createChart()
+
 } else {
 //   This shows the next three images until the total selections reach 25. 
     randomImage();
     }
+
+localStorage.setItem("imageObject", JSON.stringify(imageArray))
+console.log(localStorage)
+console.log(imageArray)
+
+localStorage.setItem("total", totalSelections)
+
+
 }
 
 // attaching event listener to image tag
